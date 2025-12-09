2c22234cee599f35ca75783aceca6ba008370639
N Datta sai ram kumar <codingdatta@gmail.com>
2025-12-09 12:37:06 +0530

    removed the client folder

--- Client/src/components/dashboards/admindashboard.jsx
+++
@@ -1,9 +0,0 @@
-// src/components/dashboards/AdminDashboard.js
-import React from "react";
-
-const AdminDashboard = () => {
-  return <div>
-    <h1>Welcome to the Admin Dashboard </h1></div>;
-};
-
-export default AdminDashboard;
