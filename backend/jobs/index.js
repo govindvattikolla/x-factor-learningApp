73f0ae8b542a770a80026af906efb2c162cc435b
N Datta sai ram kumar <codingdatta@gmail.com>
2025-12-11 17:11:27 +0530

    updated the module

--- backend/jobs/index.js
+++ backend/jobs/index.js
@@ -2,9 +2,24 @@ import connectDB from "../config/dbconfig.js";
 import dotenv from "dotenv";
 dotenv.config();
 
+const args = process.argv.slice(2);
+
+let mode = null;
+const modeIndex = args.indexOf("--mode");
+if (modeIndex !== -1) {
+    mode = args[modeIndex + 1];
+}
+
+console.log("Mode:", mode);
+
 connectDB().then(() => {
-    console.log("MongoDB Connected!")
+    console.log("MongoDB Connected!");
 });
 
-console.log('cron jobs initialized');
+console.log("cron jobs initialized");
+
 import "./image-cleanup.js";
+
+if (mode === "transcode") {
+    import("./transcodeWorker.js");
+}
